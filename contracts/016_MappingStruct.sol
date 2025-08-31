65d479205182658a1780c9b3335e2573d562c264
kkuejo <kkuejo@yahoo.co.jp>
2025-08-31 09:42:39 +0200

    20250831の更新

--- contracts/016_MappingStruct.sol
+++ contracts/016_MappingStruct.sol
@@ -20,12 +20,17 @@ contract MappingStructExample{
     mapping(address => Balance) public balances;
 
     function getDepositNum(address _from, uint _numDeposit) public view returns(Transaction memory){
+        //numDepositは、depositsのインデックスである。(つまり、入金履歴の番号)。
         return balances[_from].deposits[_numDeposit];
     }
 
     function depositMoney() public payable {
         balances[msg.sender].totalBalance += msg.value;
+        //depositは関数実行中のみに存在すれば十分なので、Storage変数ではなく、Memory変数として宣言する。
         Transaction memory deposit = Transaction(msg.value, block.timestamp);
+        //入金のTransactionをdepositsに追加する。
+        //最初のアドレスXのTransaction(金額、タイムスタンプ)をbalances[X].deposits[0]に格納する。
+        //2回目のTransaction(金額、タイムスタンプ)をbalances[X].deposits[1]に格納する。   
         balances[msg.sender].deposits[balances[msg.sender].numDeposits] = deposit;
         balances[msg.sender].numDeposits++;
     }
